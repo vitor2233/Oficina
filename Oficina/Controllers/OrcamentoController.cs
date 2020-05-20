8c3c3cec9277461cf050b69f83e7d8a541882005
vitor2233 <vitorguilherme2233@gmail.com>
2020-05-19 22:26:36 -0300

    Adicionado CRUD orçamento e serviço

--- Oficina/Controllers/OrcamentoController.cs
+++ Oficina/Controllers/OrcamentoController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,81 @@ namespace Oficina.Controllers
 {
     class OrcamentoController
     {
+        public int ID_Orcamento { get; set; }
+
+        public string TipoPagamento { get; set; }
+
+        public string Descricao { get; set; }
+
+        public int ValorPagamento { get; set; }
+
+        public string DataPagamento { get; set; }
+
+        public int Servico_ID { get; set; }
+
+        AcessoController bd = new AcessoController();
+
+        public bool Insert()
+        {
+            try
+            {
+                bd.Conectar();
+                bd.ExecutarComandosSql(String.Format("INSERT INTO tb_orcamento (tipoPagamento, descricao, valorPagamento, dataPagamento, servico_ID) VALUES ('{0}','{1}',{2},'{3}',{4})",
+                    TipoPagamento, Descricao, ValorPagamento, DataPagamento, Servico_ID));
+                bd.Desconectar();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message + "Erro ao adicionar os dados do orçamento. ");
+            }
+
+        }
+        public bool Update(int codOrcamentoClicado)
+        {
+            try
+            {
+                bd.Conectar();
+                bd.ExecutarComandosSql(String.Format("UPDATE tb_orcamento SET tipoPagamento = '{0}', descricao = '{1}', valorPagamento = {2}, dataPagamento = '{3}', servico_ID = {4}" +
+                    " WHERE ID = {4} ", TipoPagamento, Descricao, ValorPagamento, DataPagamento, codOrcamentoClicado));
+
+                bd.Desconectar();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message + "Erro ao editar os dados deste orçamento" +
+                    ". ");
+            }
+        }
+        public bool Delete(int codOrcamentoClicado)
+        {
+            try
+            {
+                bd.Conectar();
+                bd.ExecutarComandosSql(String.Format("DELETE FROM tb_orcamento WHERE ID = {0}", codOrcamentoClicado));
+                bd.Desconectar();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message + "Erro ao excluir os dados do orçamento. ");
+            }
+        }
+        public DataTable Select()
+        {
+            bd.Conectar();
+            DataTable dt = bd.RetDataTable(String.Format("SELECT * FROM tb_orcamento"));
+            bd.Desconectar();
+            return dt;
+        }
+
+        public DataTable Select(string searchText)
+        {
+            bd.Conectar();
+            DataTable dt = bd.RetDataTable(String.Format("SELECT * FROM tb_orcamento WHERE descricao LIKE '%{0}%'", searchText));
+            bd.Desconectar();
+            return dt;
+        }
     }
 }
--- Oficina/Controllers/ServicoController.cs
+++ Oficina/Controllers/ServicoController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,79 @@ namespace Oficina.Controllers
 {
     class ServicoController
     {
+        public int ID_Servico { get; set; }
+
+        public string TipoServico { get; set; }
+
+        public string Tempo { get; set; }
+
+        public string Adicional { get; set; }
+
+        public string Carro_ID { get; set; }
+
+        AcessoController bd = new AcessoController();
+
+        public bool Insert()
+        {
+            try
+            {
+                bd.Conectar();
+                bd.ExecutarComandosSql(String.Format("INSERT INTO tb_servico (tipoServico, tempo, adicional, carro_ID) VALUES ('{0}','{1}','{2}',{3})",
+                    TipoServico, Tempo, Adicional, Carro_ID));
+                bd.Desconectar();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message + "Erro ao adicionar os dados do serviço. ");
+            }
+
+        }
+        public bool Update(int codServicoClicado)
+        {
+            try
+            {
+                bd.Conectar();
+                bd.ExecutarComandosSql(String.Format("UPDATE tb_servico SET tipoServico = '{0}', tempo = '{1}', adicional ='{2}', carro_ID = {3}" +
+                    " WHERE ID = {4} ", TipoServico, Tempo, Adicional, Carro_ID, codServicoClicado));
+
+                bd.Desconectar();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message + "Erro ao editar os dados deste serviço" +
+                    ". ");
+            }
+        }
+        public bool Delete(int codServicoClicado)
+        {
+            try
+            {
+                bd.Conectar();
+                bd.ExecutarComandosSql(String.Format("DELETE FROM tb_servico WHERE ID = {0}", codServicoClicado));
+                bd.Desconectar();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message + "Erro ao excluir os dados do serviço. ");
+            }
+        }
+        public DataTable Select()
+        {
+            bd.Conectar();
+            DataTable dt = bd.RetDataTable(String.Format("SELECT * FROM tb_servico"));
+            bd.Desconectar();
+            return dt;
+        }
+
+        public DataTable Select(string searchText)
+        {
+            bd.Conectar();
+            DataTable dt = bd.RetDataTable(String.Format("SELECT * FROM tb_servico WHERE tipoServico LIKE '%{0}%'", searchText));
+            bd.Desconectar();
+            return dt;
+        }
     }
 }
